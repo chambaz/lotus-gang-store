089eb4684701e159cbda0e53ee1e426f1e568991
Adam Chambers <adammchambers@gmail.com>
2022-05-14 23:45:48 -0400

    Include JS and hook up event listener on phanomt button

---
+++ assets/custom.js
@@ -0,0 +1,7 @@
+var phantomConnectBtn = document.querySelector('[data-phantom-connect]')
+
+if (phantomConnectBtn) {
+  phantomConnectBtn.addEventListener('click', function() {
+    alert('Connect wallet')
+  })
+}
