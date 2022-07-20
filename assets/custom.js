2b1ba5acc1a96e18a07aa1bc99e580c6dba01e71
Adam Chambers <adammchambers@gmail.com>
2022-07-20 14:56:07 -0400

    Refactor wallet connect logic to work with new holders collection

--- assets/custom.js
+++ assets/custom.js
@@ -10,81 +10,47 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 const phantomConnectBtn = document.querySelector('[data-phantom-connect]');
-const lotusNftsContainer = document.querySelector('[data-lotus-nfts]');
-const productQty = document.querySelector('[data-product-qty]');
-const productBtns = document.querySelector('[data-product-btns]');
-const lotusNftChosen = document.querySelector('[data-lotus-nft-chosen]');
-const lotusNftToken = document.querySelector('[data-lotus-nft-token]');
+const checkConnectionDiv = document.querySelector('[data-connect-check]');
+
+const checkConnection = async () => {
+  try {
+    const resp = await window.solana.connect();
+    const address = resp.publicKey.toString();
+    const addressTrunc = address.substr(0, 4) + '...' + address.substr(-4);
+    connected = true;
+    console.log('Account connected: ' + address);
+    const publicAddress = await (0, _solRayz.resolveToWalletAddress)({
+      text: address
+    });
+    const nftArray = await (0, _solRayz.getParsedNftAccountsByOwner)({
+      publicAddress
+    });
+    const lotusNfts = nftArray.filter(nft => {
+      return nft.updateAuthority === '3n1mz8MyqpQwgX9E8CNPPZtAdJa3aLpuCSMbPumM9wzZ';
+    });
+    console.log(lotusNfts);
+  } catch (err) {
+    console.error('Unable to connect');
+    console.error(err);
+  }
+};
+
 let connected = false;
-let chosen = null;
+
+if (checkConnectionDiv) {
+  checkConnection();
+}
 
 if (phantomConnectBtn) {
-  productQty.style.opacity = 0;
-  productBtns.style.opacity = 0;
-  lotusNftsContainer.addEventListener('click', e => {
-    console.log(e.target.parentNode);
-
-    if (e.target.parentNode.hasAttribute('data-lotus-nft')) {
-      const name = e.target.parentNode.querySelector('[data-lotus-nft-name]').innerHTML;
-      const lotusNfts = lotusNftsContainer.querySelectorAll('[data-lotus-nft]');
-      lotusNfts.forEach(lotusNft => {
-        lotusNft.style.opacity = 0.25;
-      });
-
-      if (name != chosen) {
-        chosen = name;
-        lotusNftChosen.querySelector('span').innerHTML = name;
-        lotusNftToken.value = name;
-        e.target.parentNode.style.opacity = 1;
-        productQty.style.opacity = 1;
-        productBtns.style.opacity = 1;
-        lotusNftChosen.style.opacity = 1;
-      } else {
-        chosen = null;
-        productQty.style.opacity = 0;
-        productBtns.style.opacity = 0;
-        lotusNftChosen.style.opacity = 0;
-      }
-    }
-  });
   phantomConnectBtn.addEventListener('click', async () => {
     if (connected) {
       return;
     }
 
-    try {
-      const resp = await window.solana.connect();
-      const address = resp.publicKey.toString();
-      const addressTrunc = address.substr(0, 4) + '...' + address.substr(-4);
-      connected = true;
-      console.log('Account connected: ' + address);
-      const publicAddress = await (0, _solRayz.resolveToWalletAddress)({
-        text: address
-      });
-      const nftArray = await (0, _solRayz.getParsedNftAccountsByOwner)({
-        publicAddress
-      });
-      const lotusNfts = nftArray.filter(nft => {
-        return nft.updateAuthority === '3n1mz8MyqpQwgX9E8CNPPZtAdJa3aLpuCSMbPumM9wzZ';
-      });
-      console.log(lotusNfts);
-      const lotusNftMarkup = await Promise.all(lotusNfts.map(async lotusNft => {
-        const req = await fetch(lotusNft.data.uri);
-        const json = await req.json();
-        console.log(lotusNft);
-        console.log(json);
-        return `
-          <div class="lotus-nft" data-lotus-nft>
-            <img class="lotus-nft-img" src="${json.image}" />
-            <h2 class="lotus-nft-name" data-lotus-nft-name>${lotusNft.data.name}</h2>
-          </div>
-        `;
-      }));
-      phantomConnectBtn.querySelector('span').innerHTML = addressTrunc;
-      lotusNftsContainer.innerHTML = lotusNftMarkup.join('');
-    } catch (err) {
-      console.error('Unable to connect');
-      console.error(err);
+    await checkConnection();
+
+    if (connected) {
+      window.location = '/collections/holders';
     }
   });
 }
--- index.js
+++ index.js
@@ -5,91 +5,47 @@ import {
 } from "@nfteyez/sol-rayz"
 
 const phantomConnectBtn = document.querySelector('[data-phantom-connect]')
-const lotusNftsContainer = document.querySelector('[data-lotus-nfts]')
-const productQty = document.querySelector('[data-product-qty]')
-const productBtns = document.querySelector('[data-product-btns]')
-const lotusNftChosen = document.querySelector('[data-lotus-nft-chosen]')
-const lotusNftToken = document.querySelector('[data-lotus-nft-token]')
+const checkConnectionDiv = document.querySelector('[data-connect-check]')
+
+const checkConnection = async () => {
+  try {
+    const resp = await window.solana.connect()
+    const address = resp.publicKey.toString()
+    const addressTrunc = address.substr(0, 4) + '...' + address.substr(-4)
+    connected = true
+    console.log('Account connected: '+address)
+
+    const publicAddress = await resolveToWalletAddress({
+      text: address
+    })
+    const nftArray = await getParsedNftAccountsByOwner({
+      publicAddress
+    })
+    const lotusNfts = nftArray.filter(nft => {
+      return nft.updateAuthority === '3n1mz8MyqpQwgX9E8CNPPZtAdJa3aLpuCSMbPumM9wzZ'
+    })  
+    console.log(lotusNfts)
+  } catch (err) {
+    console.error('Unable to connect')
+    console.error(err)
+  }
+}
 
 let connected = false
-let chosen = null
+
+if (checkConnectionDiv) {
+  checkConnection()
+}
 
 if (phantomConnectBtn) {
-  productQty.style.opacity = 0
-  productBtns.style.opacity = 0
-
-  lotusNftsContainer.addEventListener('click', (e) => {
-    console.log(e.target.parentNode)
-    if (e.target.parentNode.hasAttribute('data-lotus-nft')) {
-      const name = e.target.parentNode.querySelector('[data-lotus-nft-name]').innerHTML
-      const lotusNfts = lotusNftsContainer.querySelectorAll('[data-lotus-nft]')
-      
-      lotusNfts.forEach(lotusNft => {
-        lotusNft.style.opacity = 0.25
-      })
-
-      if (name != chosen) {
-        chosen = name
-        lotusNftChosen.querySelector('span').innerHTML = name
-        lotusNftToken.value = name
-
-        e.target.parentNode.style.opacity = 1
-        productQty.style.opacity = 1
-        productBtns.style.opacity = 1
-        lotusNftChosen.style.opacity = 1
-      } else {
-        chosen = null
-
-        productQty.style.opacity = 0
-        productBtns.style.opacity = 0
-        lotusNftChosen.style.opacity = 0
-      }
-      
-    }
-  })
-
   phantomConnectBtn.addEventListener('click', async () => {
     if (connected) {
       return
     }
-    try {
-      const resp = await window.solana.connect()
-      const address = resp.publicKey.toString()
-      const addressTrunc = address.substr(0, 4) + '...' + address.substr(-4)
-      connected = true
-      console.log('Account connected: '+address)
+    await checkConnection()
 
-      const publicAddress = await resolveToWalletAddress({
-        text: address
-      })
-      const nftArray = await getParsedNftAccountsByOwner({
-        publicAddress
-      })
-      const lotusNfts = nftArray.filter(nft => {
-        return nft.updateAuthority === '3n1mz8MyqpQwgX9E8CNPPZtAdJa3aLpuCSMbPumM9wzZ'
-      })  
-      console.log(lotusNfts)
-
-      const lotusNftMarkup = await Promise.all(lotusNfts.map(async lotusNft => {
-        const req = await fetch(lotusNft.data.uri)
-        const json = await req.json()
-        console.log(lotusNft)
-        console.log(json)
-
-        return `
-          <div class="lotus-nft" data-lotus-nft>
-            <img class="lotus-nft-img" src="${json.image}" />
-            <h2 class="lotus-nft-name" data-lotus-nft-name>${lotusNft.data.name}</h2>
-          </div>
-        `
-      }))
-
-      phantomConnectBtn.querySelector('span').innerHTML = addressTrunc
-      lotusNftsContainer.innerHTML = lotusNftMarkup.join('')
-
-    } catch (err) {
-      console.error('Unable to connect')
-      console.error(err)
+    if (connected) {
+      window.location = '/collections/holders'
     }
   })
 }
